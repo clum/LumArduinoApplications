3c058d15bff147e7f2dd4458de37d4cd13e88fb0
Christopher Lum <christopher.w.lum@outlook.com>
2022-03-30 08:22:42 -0700

    updates

--- Applications/SpaceShrimp/DataLogger/DataAnalysis/Step01_LoadData.m
+++ Applications/SpaceShrimp/DataLogger/DataAnalysis/Step01_LoadData.m
@@ -6,6 +6,7 @@
 %Version History
 %12/09/21: Created
 %03/21/22: Cleaned up and generalized
+%03/29/22: Cleaned up
 
 clear
 clc
@@ -15,26 +16,23 @@ tic
 
 ChangeWorkingDirectoryToThisLocation();
 
-% dataFile = '.\21_12_29\LOG03.csv';
-% 
-% T = readtable(dataFile);
-% data = table2array(T);
-% 
-% t_s = data(:,1)./1000;
-% T1_f = CelciustoFarenheit(data(:,2));
-% T2_f = CelciustoFarenheit(data(:,3));
-% 
-% figure
-% hold on
-% plot(t_s,T1_f)
-% plot(t_s,T2_f)
-% grid on
-
 %% User selections
-dataFile        = '.\22_03_21\LOG05.csv';
-dataStartRow    = 6;
-dataEndRow      = 285;
-outputFile      = 'Step01Results.mat';
+% dataFile        = '.\22_03_21\LOG05.csv';
+% dataStartRow    = 6;
+% dataEndRow      = 285;
+% outputFile      = '22_03_21_LOG5_ID01.mat'; 
+
+% %All data from hide and seek 1 (includes prep/testing)
+% dataFile        = '.\22_03_28\LOG05.csv';
+% dataStartRow    = 1;
+% dataEndRow      = 2266;
+% outputFile      = '22_03_28_LOG5_ID01.mat'; 
+
+%data just isolated to hide and seek 1 excursion
+dataFile        = '.\22_03_28\LOG05.csv';
+dataStartRow    = 102;
+dataEndRow      = 2266;
+outputFile      = '22_03_28_LOG5_ID02.mat'; 
 
 %% Load data
 T = readtable(dataFile);
--- Applications/SpaceShrimp/DataLogger/DataAnalysis/Step02_AnalyzeData.m
+++ Applications/SpaceShrimp/DataLogger/DataAnalysis/Step02_AnalyzeData.m
@@ -15,7 +15,9 @@ tic
 ChangeWorkingDirectoryToThisLocation();
 
 %% User selections
-inputFile   = 'Step01Results.mat';
+% inputFile   = '22_03_21_LOG5_ID01.mat';
+% inputFile   = '22_03_28_LOG5_ID01.mat';
+inputFile   = '22_03_28_LOG5_ID02.mat';
 
 %% Load data
 temp = load(inputFile);
@@ -23,28 +25,35 @@ data = temp.data;
 
 %% Analyze and plot results
 gps_isValid             = data(:,1);
-gps_date_year           = data(:,2);
-gps_date_month          = data(:,3);
-gps_date_day            = data(:,4);
-gps_time_hour           = data(:,5);
-gps_time_minute         = data(:,6);
-gps_time_second         = data(:,7);
-gps_satellites_value2   = data(:,8);
-temperatureA_c          = data(:,9);
-temperatureB_c          = data(:,10);
-gps_location_lat_deg    = data(:,11);
-gps_location_lng_deg    = data(:,12);
-gps_speed_mph           = data(:,13);
-gps_altitude_ft         = data(:,14);
-gps_hdop_fraction       = data(:,15);
 
 %Isolate valid values
 validIndices = find(gps_isValid==1);
+
+gps_date_year           = data(validIndices,2);
+gps_date_month          = data(validIndices,3);
+gps_date_day            = data(validIndices,4);
+gps_time_hour           = data(validIndices,5);
+gps_time_minute         = data(validIndices,6);
+gps_time_second         = data(validIndices,7);
+gps_satellites_value2   = data(validIndices,8);
+temperatureA_c          = data(validIndices,9);
+temperatureB_c          = data(validIndices,10);
+gps_location_lat_deg    = data(validIndices,11);
+gps_location_lng_deg    = data(validIndices,12);
+gps_speed_mph           = data(validIndices,13);
+gps_altitude_ft         = data(validIndices,14);
+gps_hdop_fraction       = data(validIndices,15);
 
 %Compute time
 time = datetime(gps_date_year,gps_date_month,gps_date_day,gps_time_hour,gps_time_minute,gps_time_second);
 
 %% Plots
+%Plot time so we have an idea how many sets of data are in the log file
+figure
+plot([1:1:length(time)],time)
+ylabel('time')
+grid on
+
 figure
 subplot(5,1,1)
 hold on
@@ -76,7 +85,7 @@ grid on
 
 %Plot trajectory
 figure
-geoplot(gps_location_lat_deg(validIndices),gps_location_lng_deg(validIndices))
+geoplot(gps_location_lat_deg,gps_location_lng_deg,'LineWidth',3)
 geobasemap('streets')
 
 toc
